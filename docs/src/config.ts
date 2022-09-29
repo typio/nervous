6c6c0a573218af43e803bb38f6fe68492dcfa407
Thomas Huber <me@tohuber.com>
2022-09-29 02:34:28 -0700

    Started trying mnist

--- docs/src/config.ts
+++ docs/src/config.ts
@@ -20,7 +20,7 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/typio/light/tree/main/apps/docs`;
+export const GITHUB_EDIT_URL = `https://github.com/typio/light/tree/main/docs`;
 
 export const COMMUNITY_INVITE_URL = ``;
 
