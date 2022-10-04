7441040d34238b9c2558aa4ea41e22da9a6030e6
Thomas Huber <me@tohuber.com>
2022-10-04 11:38:26 -0700

    Sync :rocket:

--- demos/src/main.ts
+++ demos/src/main.ts
@@ -1,32 +1,57 @@
 import pako from 'pako'
 import p5 from "p5"
 
-import trainImagesFile from './t10k-images-idx3-ubyte.gz.data'
-import labelLabelsFile from './t10k-labels-idx1-ubyte.gz.data'
+import * as lt from "light"
 
 
-let res = pako.ungzip(trainImagesFile)
+// import trainImagesFile from './t10k-images-idx3-ubyte.gz.data'
+// import labelLabelsFile from './t10k-labels-idx1-ubyte.gz.data'
 
-const s = p => {
+let trainImagesFilePromise = import('./t10k-images-idx3-ubyte.gz.data')
+let trainlabelLabelsPromise = import('./t10k-labels-idx1-ubyte.gz.data')
+
+
+// trainImagesFilePromise.then((result) => {
+//     console.log(result.default)
+// })
+
+
+const s = (p) => {
+
+
+
     let pixelValues = []
 
-    let dataFileBuffer = pako.ungzip(trainImagesFile)
-    let labelFileBuffer = pako.ungzip(labelLabelsFile)
+    trainImagesFilePromise.then((data) => {
+        let dataFileBuffer = pako.ungzip(data.default)
+        trainlabelLabelsPromise.then((data) => {
+            let labelFileBuffer = pako.ungzip(data.default)
 
-    for (let image = 0; image <= 1000; image++) {
-        let pixels = []
+            for (let image = 0; image <= 1000; image++) {
+                let pixels = []
 
-        for (let y = 0; y < 28; y++) {
-            for (let x = 0; x < 28; x++) {
-                pixels.push(dataFileBuffer[(image * 28 * 28) + (x + (y * 28)) + 16])
+                for (let y = 0; y < 28; y++) {
+                    for (let x = 0; x < 28; x++) {
+                        pixels.push(dataFileBuffer[(image * 28 * 28) + (x + (y * 28)) + 16])
+                    }
+                }
+
+                let imageData = {}
+                imageData[JSON.stringify(labelFileBuffer[image + 8])] = pixels
+
+                pixelValues.push(imageData)
+
             }
-        }
 
-        let imageData = {}
-        imageData[JSON.stringify(labelFileBuffer[image + 8])] = pixels
+        })
 
-        pixelValues.push(imageData)
-    }
+    })
+
+
+
+
+
+
 
     let gp
     p.setup = () => {
@@ -34,10 +59,16 @@ const s = p => {
         gp = p.createGraphics(p.width, p.height)
         gp.pixelDensity(1)
         p.noLoop()
+
+        window.document.getElementById('loading_msg').style = 'display: none;'
+
+
     }
 
     p.draw = function () {
         p.clear(0, 0, 0, 0)
+
+
         for (let image = 0; image <= 1000; image++) {
             gp.reset()
             gp.loadPixels()
@@ -54,6 +85,9 @@ const s = p => {
             p.image(gp, Math.floor(image % 40) * 28, Math.floor(image / 40) * 50, p.width, p.height)
             p.text(Object.keys(pixelValues[image])[0], 9 + Math.floor(image % 40) * 28, Math.floor(image / 40) * 50 + 40)
         }
+
+
+
     }
 }
 
