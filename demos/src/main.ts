0fb73465f5bdbce630487d3c6b571545e0b26668
Thomas Huber <me@tohuber.com>
2022-10-04 00:20:17 -0700

    No more js frameworks :nauseated_face:

--- demos/src/main.ts
+++ demos/src/main.ts
@@ -1,61 +1,65 @@
 import p5 from "p5"
 
-import trainImagesFile from './t10k-images.idx3-ubyte?raw';
-import labelLabelsFile from './t10k-labels.idx1-ubyte?raw';
+// import trainImagesFile from './t10k-images.idx3-ubyte?raw'
+// import labelLabelsFile from './t10k-labels.idx1-ubyte?raw'
 
 const s = p => {
-    let pixelValues = [];
+    let pixelValues = []
 
-    let dataFileBuffer = new ArrayBuffer(trainImagesFile.length);
-    let labelFileBuffer = new ArrayBuffer(labelLabelsFile.length);
+    // let dataFileBuffer = new ArrayBuffer(trainImagesFile.length)
+    // let labelFileBuffer = new ArrayBuffer(labelLabelsFile.length)
 
-    for (let i = 0; i < trainImagesFile.length; i++) {
-        dataFileBuffer[i] = trainImagesFile.charCodeAt(i);
-        labelFileBuffer[i] = labelLabelsFile.charCodeAt(i);
-    }
+    // for (let i = 0; i < trainImagesFile.length; i++) {
+    //     dataFileBuffer[i] = trainImagesFile.charCodeAt(i)
+    //     labelFileBuffer[i] = labelLabelsFile.charCodeAt(i)
+    // }
 
-    for (let image = 0; image <= 200; image++) {
-        let pixels = [];
+    // for (let image = 0; image <= 400; image++) {
+    //     let pixels = []
 
-        for (let y = 0; y <= 27; y++) {
-            for (let x = 0; x <= 27; x++) {
-                pixels.push(dataFileBuffer[(image * 28 * 28) + (x + (y * 28)) + 15]);
-            }
-        }
+    //     for (let y = 0; y < 28; y++) {
+    //         for (let x = 0; x < 28; x++) {
+    //             pixels.push(dataFileBuffer[(image * 28 * 28) + (x + (y * 28)) + 16])
+    //         }
+    //     }
 
-        let imageData = {};
-        imageData[JSON.stringify(labelFileBuffer[image + 8])] = pixels;
+    //     let imageData = {}
+    //     imageData[JSON.stringify(labelFileBuffer[image + 8])] = pixels
 
-        pixelValues.push(imageData);
-    }
+    //     pixelValues.push(imageData)
+    // }
 
-    let gp;
+    let gp
     p.setup = () => {
-        p.createCanvas(560, 1000);
-        gp = p.createGraphics(p.width, p.height);
-        gp.pixelDensity(1);
-        p.noLoop();
-    };
+        p.createCanvas(1120, 1000)
+        gp = p.createGraphics(p.width, p.height)
+        gp.pixelDensity(1)
+        p.noLoop()
+    }
 
     p.draw = function () {
-        p.clear(0, 0, 0, 0);
-        for (let image = 0; image < 200; image++) {
+        p.clear(0, 1, 0, 0)
+        p.fill(0, 0, 255)
+        p.strokeWeight(5);
+        p.stroke(255,0,0)
+        p.rect(30, 20, 55, 55, 15);
+        // for (let image = 0; image <= 400; image++) {
+        //     gp.reset()
+        //     gp.loadPixels()
+        //     for (let x = 0; x < 28; x++) {
+        //         for (let y = 0; y < 28; y++) {
+        //             let index = (p.width * y + x) * 4
+        //             gp.pixels[index + 0] = Object.values(pixelValues[image])[0][(28 * y) + x]
+        //             gp.pixels[index + 1] = Object.values(pixelValues[image])[0][(28 * y) + x]
+        //             gp.pixels[index + 2] = Object.values(pixelValues[image])[0][(28 * y) + x]
+        //             gp.pixels[index + 3] = 255
+        //         }
+        //     }
+        //     gp.updatePixels()
+        //     p.image(gp, Math.floor(image % 40) * 28, Math.floor(image / 40) * 50, p.width, p.height)
+        //     p.text(Object.keys(pixelValues[image])[0], 9 + Math.floor(image % 40) * 28, Math.floor(image / 40) * 50 + 40)
+        // }
+    }
+}
 
-            gp.loadPixels();
-            for (let x = 0; x < 28; x++) {
-                for (let y = 0; y < 28; y++) {
-                    let index = (p.width * y + x) * 4;
-                    gp.pixels[index + 0] = Object.values(pixelValues[image])[0][28 * y + x];
-                    gp.pixels[index + 1] = Object.values(pixelValues[image])[0][28 * y + x];
-                    gp.pixels[index + 2] = Object.values(pixelValues[image])[0][28 * y + x];
-                    gp.pixels[index + 3] = 255;
-                }
-            }
-            gp.updatePixels();
-            p.image(gp, Math.floor(image%20) * 28, Math.floor(image / 20)*50, p.width, p.height);
-            p.text(Object.keys(pixelValues[image])[0], 9 + Math.floor(image%20) * 28 , Math.floor(image / 20)*50+40);
-        }
-    };
-};
-
-new p5(s); 
+new p5(s) 
